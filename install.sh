33e0903d20e2a8b56346e0acafb0e4749eff99c4
Roman Schader <r.schader@reply.de>
2019-02-27 14:28:02 +0100

    update install.sh to simlink ideavimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
