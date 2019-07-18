3ca81f6e47c2b119eedf046400cd3e2359ff721a
Roman Schader <r.schader@reply.de>
2019-07-18 16:16:26 +0200

    cleanup install sh

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-# Vim (~/.vim/autoload)
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+set -o errexit
+set -o nounset
+
+println() { printf  "$1\n"; }
+
+cleanup() {
+    println ""
+    println "some error occured => cleaning up ..."
+    println "deleting symlinks ... "
+    rm -rf ~/.vimrc
+    rm -rf ~/.config/nvim/init.vim
+    rm -rf ~/.ideavimrc
+    rm -rf ~/.tmux.conf
+    rm -rf ~/.bash_profile
+    rm -rf ~/.bash_aliases
+    rm -rf ~/.bashrc
+    println "cleanup done"
+    exit 1
+}
+
+# echo an error message before exiting
+trap cleanup EXIT
 
 # Neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -14,8 +34,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt-get install xclip
 
 if [ ! -d "~/.config/nvim" ]; then
-	mkdir -p ~/.config/nvim
-	ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim
+    mkdir -p ~/.config/nvim
+    ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim
 fi
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
